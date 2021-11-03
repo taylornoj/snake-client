c09bcbaa0551f88f405d27a24b2f0459a0c7447a
Taylor Noj <taylornoj@gmail.com>
2021-11-03 18:52:29 +0000

    created client.js and copied conn function

--- play.js
+++ play.js
@@ -1,28 +1,28 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on('connect', () => {
-    console.log('Connected!');
-  });
+//   conn.on('connect', () => {
+//     console.log('Connected!');
+//   });
 
  
-  conn.on('data', (data) => {
-    console.log(data);
-  });
+//   conn.on('data', (data) => {
+//     console.log(data);
+//   });
   
 
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
