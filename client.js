47d18503961c1b92aed4e9e2dfe0a10d6738160d
Taylor Noj <taylornoj@gmail.com>
2021-11-03 22:22:55 +0000

    settimeout to have my name appear each time and practiced moving snek

--- client.js
+++ client.js
@@ -6,21 +6,32 @@ const connect = function () {
     host: "165.227.47.243",
     port: 50541
   });
-
+  
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on('connect', () => {
-    conn.write('Name: TVN'),
-    conn.write('Move: up'),
-    console.log('Successfully connected to game server!');
-  });
-
- 
   conn.on('data', (data) => {
     console.log(data);
   });
-  
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    setTimeout(() => {
+      conn.write("Name: TVN");
+    }, 100);
+  });
+
+  // conn.on("connect", () => {
+  //   conn.write("Move: down");
+  //   setTimeout(() => {
+  //     conn.write("Move: left")
+  //   }, 500);
+  //   setInterval(() => {
+  //     conn.write("Move: up")
+  //   }, 50);
+  //   conn.write("Move: left");
+  // })
+
   return conn;
 };
 
