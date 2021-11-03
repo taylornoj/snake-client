14ff4126947fb16d3682777d09a27ee047ee8550
Taylor Noj <taylornoj@gmail.com>
2021-11-03 22:53:19 +0000

    adding ctrl c command

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 //const net = require("net");
 const {connect} = require('./client')
 
+// setup interface to handle user input from stdin
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (data) => {
+  handleUserInput(data);
+  });
+
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
 console.log("Connecting ...");
 connect();
